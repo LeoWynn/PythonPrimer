55f485f5731431fa434089cfdcc10718b7620d10
LeoWynn <leo_wm_wen@163.com>
2017-07-06 21:54:26 +0800

    四种翻转字符串/列表的方式

---
+++ reverse_usage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+
+'''Module: reverse_usage
+Created by Leo Wen on 2017-07-06 21:44:23
+四种翻转字符串/列表的方式
+'''
+
+
+def test():
+    '''Test'''
+    print 'Please test here.'
+    # 翻转列表本身
+    testList = [1, 3, 5]
+    testList.reverse()
+    print(testList)
+    
+    #在一个循环中翻转并迭代输出
+    for element in reversed([1,3,5]):
+        print(element)
+
+    #使用切片翻转列表
+    print [1, 3, 5][::-1]
+
+    #一行代码翻转字符串
+    print "Test Python"[::-1]
+
+
+if __name__ == '__main__':
+    test()
